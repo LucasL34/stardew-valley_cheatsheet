61a01c43950602de7417ebb0fd04957455895358
Lucas <lukaslopezlp@gmail.com>
2023-08-19 22:15:19 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-xl text-[#c4c4c4]">Hello, Next.js!</h1>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus tempore
+        aspernatur quam praesentium sunt totam asperiores numquam, amet eius
+        quidem, hic nesciunt obcaecati corrupti ratione vel, voluptas veritatis
+        ullam id.
+      </p>
+    </>
+  );
+}
